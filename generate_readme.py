5f1de712b7d286076df437edaf6d7a8d1b46c636
nOctaveLay <wjddkdml1124@naver.com>
2024-09-05 02:09:10 +0900

    generate

--- generate_readme.py
+++ generate_readme.py
@@ -18,33 +18,40 @@ def generate_paper(change_contents:str, text_file_path = "./README.md"):
             i += 1
     print(new_contents)
 
-def generate_retrospects(text_file_path = "./README.md"):
+# 임시 함수
+def add_new_contents(find_string:str, text_file_path = "./README.md"):
     """
-    "README.md"의 retrospect의 날짜 
     '### 24.09.02 MON - 24.09.06 FRI'를 만듬
     
     """
-    new_contents = ''
     now = datetime.now()
     end = now + timedelta(days = 5)
+    new_contents = ''
+
+    date_format = f"### {now.strftime('%y.%m.%d')} {now.strftime('%a').upper()} - {end.strftime('%y.%m.%d')} {end.strftime('%a').upper()}\n\n"
+    name_format = '- **📍Hello**\n- **📍IsSomeone**\n- **📍HowAreYou**\n- **📍AndSomeone**\n- **📍Isasking**\n- **📍Someone**\n\n'
+    put_string = date_format + name_format
 
     with open(text_file_path,'r',encoding = 'utf-8') as f:
         lines = f.readlines()
         i = 0
         while i < len(lines):
             new_string = lines[i].strip()
+
             # 주간 정리 위에 올릴 예정
-            if new_string == f"## 📝주간 정리 (optional)":
-                string = f"### {now.strftime('%y.%m.%d')} {now.strftime('%a').upper()} - {end.strftime('%y.%m.%d')} {end.strftime('%a').upper()}\n\n"
-                new_contents += string
-                new_contents += '- **📍Hello**\n- **📍IsSomeone**\n- **📍HowAreYou**\n- **📍AndSomeone**\n- **📍Isasking**\n- **📍Someone**\n\n'
+            if new_string == find_string:
+                new_contents += put_string
                 new_contents += "".join(lines[i:])
-                print(new_contents)
                 break
             else:
                 new_contents += new_string + "\n"
             i += 1
-            
-    print(new_contents)
 
-generate_retrospects()
+    with open(text_file_path,'w',encoding='utf-8') as f:
+        f.write(new_contents)
+
+def update_week():
+    add_new_contents("## 📝주간 정리 (optional)")
+    add_new_contents("## 주의 사항")
+
+update_week()
