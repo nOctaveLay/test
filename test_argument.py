6fad01be4cbcc4403ab73ba14652d7416f1142f8
nOctaveLay <wjddkdml1124@naver.com>
2024-09-08 22:36:11 +0900

    hello, world!

--- test_argument.py
+++ test_argument.py
@@ -3,6 +3,8 @@ parser = argparse.ArgumentParser(description = "Hello, World!")
 parser.add_argument('-d','--documents')
 
 if __name__ == "__main__":
-    args = parser.parse_args('-d -1'.split())
-    with open("test.md","w") as f:
-        f.write(args.documents)
+    args = parser.parse_args()
+    print(args)
+    if args.documents is not None:
+        with open("test.md","w") as f:
+            f.write(args.documents)
