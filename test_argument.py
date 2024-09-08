677fc0c4ffea86a0ba5f3cbd054b6e42dceed1f4
nOctaveLay <wjddkdml1124@naver.com>
2024-09-08 22:50:25 +0900

    gogogo!

--- test_argument.py
+++ test_argument.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     a = args.documents if args.documents is not None else "-1"
     os.system(f'echo {a}')
+    b = a.split("/")
+    os.system(f'echo {b}')
     # if args.documents is not None:
     #     with open("test.md","w") as f:
     #         f.write(args.documents)
